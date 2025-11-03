866f3ce3f314be97dce06d06c790c2f10cd040c9
Sarachwattanagarunwong <67160059@kmitl.ac.th>
2025-11-03 18:25:40 +0000

    ex04

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -1,10 +1,9 @@
-#1/bin/bash
-if [ $# -eq 0 ]; then
-  echo "No arguments supplied"
-  exit 1
-fi 
+#!/bin/bash
 
-for arg in "$@"
-do
-  mkdir "ex$arg"
-done
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+else
+    echo $1
+    echo $2
+    echo $3
+fi
